7f81bd74fd8a8ced26f694aaf2848ea0450fd62c
Marco De Jesus <Marco_De_Jesus@epam.com>
2020-06-07 05:18:03 -0500

    Saving to MongoDb from Html form.

--- controllers/courses.js
+++ controllers/courses.js
@@ -4,13 +4,28 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const CourseModel = mongoose.model("Course");
 
-router.get("/list", (req, res) => {
-    // Setting
-    /*var course = new CourseModel();
-    course.courseName = "NodeJs";
-    course.courseId = "2";
-    course.save();*/
+router.get("/add", (req, res) => {
+    res.render("add-course");
+});
 
+router.post("/add", (req, res) => {
+
+    var course = new CourseModel();
+    course.courseName = req.body.courseName;
+    course.courseDuration = req.body.courseDuration;
+    course.courseFee = req.body.courseFee;
+    course.courseId = Math.ceil(Math.random() * 100000000);
+
+    course.save((err, doc) => {
+        if(!err){
+            res.redirect("/courses/list");
+        }else{
+            res.send("Error Ocurred.");
+        }
+    });
+})
+
+router.get("/list", (req, res) => {
     //Getting
     CourseModel.find((err, docs) => {
         if(!err){
