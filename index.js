72e3c8a05c0670960a3eb3d8bfbbeaf40c53c761
Marco De Jesus <Marco_De_Jesus@epam.com>
2020-06-07 03:34:39 -0500

    Creating connection from model

--- index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/Edureka", { useNewUrlParser: true }, (error) =>{
-    if(!error){
-        console.log("Success Connected!");
-    }else{
-        console.log("Error connecting to database.");
-    }
-}); 
