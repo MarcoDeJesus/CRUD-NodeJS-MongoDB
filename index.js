1969a8ec75d8718468aacf03d786f8b075e101ba
Marco De Jesus <Marco_De_Jesus@epam.com>
2020-06-07 03:26:51 -0500

    Using nodemon to avoid server restarting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/Edureka", { useNewUrlParser: true }, (error) =>{
     if(!error){
-        console.log("Success!");
+        console.log("Success Connected!");
     }else{
         console.log("Error connecting to database.");
     }
