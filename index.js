b9803c3de3938c2cd9100b8251f46a996a6db6d0
Marco De Jesus <Marco_De_Jesus@epam.com>
2020-06-07 03:41:29 -0500

    Setting up the webserver with Express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const connection = require("./model");
+const express = require("express");
+const application = express();
+const path = require("path");
+const expressHandlebars = require("express-handlebars");
+const bodyparser = require  ("body-parser");
+
+application.use(bodyparser.urlencoded({
+    extended : true
+}));
+
+application.get("/", (req, res) => {
+    res.send('<h1>Hello World</h1>');
+});
+
+application.listen("3000", () => {
+    console.log("Server started.");
+});
